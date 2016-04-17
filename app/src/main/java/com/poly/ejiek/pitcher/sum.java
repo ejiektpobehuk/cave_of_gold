7a72ab1878c0f995bdf8cb81992372ff97beab20
Vlad Petrov <ejiek@mail.ru>
2016-04-17 16:30:38 +0300

    add simple Android app

---
+++ app/src/main/java/com/poly/ejiek/pitcher/sum.java
@@ -0,0 +1,12 @@
+package com.poly.ejiek.pitcher;
+
+/**
+ * Created by ejiek on 4/16/16.
+ */
+public class sum {
+
+    public int sum(int x, int y) {
+        // add x to y and return the result
+        return x + y;
+    }
+}
