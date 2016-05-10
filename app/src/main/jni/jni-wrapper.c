0fec94538e9ceaf4dbfc763be9555def086edca5
Vlad Petrov <ejiek@mail.ru>
2016-05-10 06:36:07 +0300

    jni calling function

--- app/src/main/jni/jni-wrapper.c
+++ app/src/main/jni/jni-wrapper.c
@@ -1,9 +1,13 @@
 #include <jni.h>
 
+const char * some_c_func(){
+    return "9 from deep jni";
+}
+
 JNIEXPORT jstring JNICALL
 Java_com_poly_ejiek_pitcher_MainActivity_getMsgFromJni(JNIEnv *env, jobject instance) {
 
    // TODO
 
-   return (*env)->NewStringUTF(env, "9 from jni");
+   return (*env)->NewStringUTF(env, some_c_func());
 }
